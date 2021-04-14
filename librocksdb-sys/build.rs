379c2d7f2a15fe31d2ec2726f4b6179de5c8c287
Roman Zeyde <me@romanzey.de>
2021-04-14 16:14:16 +0300

    Revert "Use matches! to fix Clippy warning (#473)"
    This reverts commit 3e223d15919e4e31e50af0dc3d52439a4635852c.

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -155,14 +155,10 @@ fn build_rocksdb() {
         lib_sources = lib_sources
             .iter()
             .cloned()
-            .filter(|file| {
-                !matches!(
-                    *file,
-                    "port/port_posix.cc"
-                        | "env/env_posix.cc"
-                        | "env/fs_posix.cc"
-                        | "env/io_posix.cc"
-                )
+            .filter(|file| match *file {
+                "port/port_posix.cc" | "env/env_posix.cc" | "env/fs_posix.cc"
+                | "env/io_posix.cc" => false,
+                _ => true,
             })
             .collect::<Vec<&'static str>>();
 
